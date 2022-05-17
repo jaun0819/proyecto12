d9c5dbe8c391dc68f2321413f25f0ab9f06879e3
jaun0819 <aguileraaraquea@gmail.com>
2022-05-17 11:25:09 -0500

    cambios del desplazamiento

--- desplaza/js/desplzamiento.js
+++ desplaza/js/desplzamiento.js
@@ -1,12 +1,17 @@
 const toyota1 =document.getElementById("toyota1");
 const toyota3 =document.getElementById("toyota3");
 const botonAde =document.getElementById("fechaD");
-
+const botonAd =document.getElementById("fechaI");
 
 botonAde.addEventListener("click",function(){
     toyota1.classList.add("toyota1-oculta");
     toyota1.classList.remove("toyota1-visible");
     toyota3.classList.add("toyota3-visible");
     toyota3.classList.remove("toyota3-oculta");
-    
-})
+});
+botonAd.addEventListener("click",function(){
+    toyota1.classList.remove("toyota1-oculta");
+    toyota1.classList.add("toyota1-visible");
+    toyota3.classList.remove("toyota3-visible");
+    toyota3.classList.add("toyota3-oculta");
+});
